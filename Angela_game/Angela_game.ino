e5dd0a1d7730ef87840d77a8872a0dbb3553d314
Luca Giacometti <luca.giacometti@barsanti.edu.it>
2019-01-24 22:56:53 +0100

    Programma per monitor seriale completo.

--- Angela_game/Angela_game.ino
+++ Angela_game/Angela_game.ino
@@ -12,15 +12,20 @@ void inserisciMeta()
   {
     if(Serial.available() > 0)
         {
-          if(Serial.readString().toInt() > 30 && Serial.readString().toInt() < 100)
+          int input = Serial.readString().toInt();
+          if(input > 30 && input < 100)
           {
-            meta = Serial.readString().toInt();
-            Serial.println(meta);
+            meta = input;
+            Serial.print("\r\n");
+            Serial.print(meta);
             appoggio = true;
           }
           else
           {
-            Serial.println("Meta non compresa tra 30 e 100!");
+            Serial.print("\r\n");
+            Serial.print("Meta non compresa tra 30 e 100!");
+            Serial.print("\r\n");
+            Serial.print("Inserire la meta compresa tra 30 e 100");
           }
         }
   }
@@ -29,7 +34,8 @@ void inserisciMeta()
 
 void attendiPuntata()
 {
-  Serial.println("Inserire la puntata");
+  Serial.print("\r\n");
+  Serial.print("Inserire la puntata");
   while(appoggio == false)
   {
     if(Serial.available() > 0 && turno == 0)
@@ -38,25 +44,78 @@ void attendiPuntata()
       if(puntataScelta > -1 && puntataScelta < 7)         //puntata compresa tra 0 e 6 se è il primo turno
       {
         puntata = puntataScelta;
-        totale = totale + puntata;
+        totale = totale + puntataScelta;
         appoggio = true;
         turno = turno + 1;
       }
       else
       {
-        Serial.println("La puntata deve essere compresa tra 0 e 6");
+        Serial.print("\r\n");            
+        Serial.print("La prima puntata deve essere compresa tra 0 e 6");
       }
     }
-    else if(Serial.available() > 0)
+    if (Serial.available() > 0 && turno != 0)
     {
       int puntataScelta = Serial.readString().toInt();
       if(puntataScelta > 0 && puntataScelta < 7 && puntataScelta != puntata && puntataScelta != 7-puntata)
       {
         puntata = puntataScelta;
+        totale = totale + puntataScelta;
+        appoggio = true;
+        turno = turno + 1;
+      }
+      else
+      {
+        Serial.print("\r\n");
+        Serial.print("La puntata deve essere compresa tra 1 e 6, diversa dalla puntata dell'utente precedente e al suo complementare a 7");
       }
     }
   }
   appoggio = false;
+}
+
+void check()      //controlla se il gioco è finito, se si ha raggiunto o superato la meta
+{
+  if(totale > meta)
+  {
+    if(turno%2 == 0) //se è un numero pari è il turno del secondo utente
+    {
+      String firstMessage = "Hai superato la meta, ha vinto il secondo utente in ";
+      String secondMessage = firstMessage + turno;
+      String message = secondMessage + " turni!";
+      Serial.print("\r\n");
+      Serial.print(message);
+    }
+    else
+    {
+      String firstMessage = "Hai superato la meta, ha vinto il primo utente in ";
+      String secondMessage = firstMessage + turno;
+      String message = secondMessage + " turni!";
+      Serial.print("\r\n");
+      Serial.print(message);
+    }
+    appoggio = true;
+  }
+  else if(totale == meta)
+  {
+    if(turno%2 == 0)
+    {
+        String firstMessage = "Hai raggiunto la meta! Vince il secondo utente in ";
+        String secondMessage = firstMessage + turno;
+        String message = secondMessage + " turni!";
+        Serial.print("\r\n");
+        Serial.print(message);
+    }
+    else
+    {
+        String firstMessage = "Hai raggiunto la meta! Vince il primo utente in ";
+        String secondMessage = firstMessage + turno;
+        String message = secondMessage + " turni!";
+        Serial.print("\r\n");
+        Serial.print(message);
+    }
+    appoggio = true;
+  }
 }
 
 void setup() {
@@ -67,4 +126,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
       inserisciMeta();
+      while(appoggio == false)
+      {
+      attendiPuntata();
+      check();
+      }
+      while(appoggio == true)
+      {
+        //per adesso blocco il programma in questo loop, dopo con arduino verrà implementato un bottone reset per iniziare una nuova partita
+      }
     }
